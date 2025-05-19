761cb30e63070391a89328f4233088dacc69c75b
David Tiechman <d0534110171@gmail.com>
2025-05-19 14:54:01 +0300

    only one class

--- Tool.cs
+++ Tool.cs
@@ -8,5 +8,21 @@ namespace Toolbox
 {
     internal class Tool
     {
+        string Name;
+        int Weghte;
+        public Tool(string name,int weghte)
+        {
+            this.Name = name;
+            this.Weghte = weghte;
+        }
+        public void Descride()
+        {
+            Console.WriteLine();
+
+        }
+        public void Use()
+        {
+            Console.WriteLine("Tool is being used");
+        }
     }
 }
